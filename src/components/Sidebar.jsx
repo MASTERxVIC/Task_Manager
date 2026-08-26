dee9bae74d31ae4578f25a892503445f7e691423
Atul Kumar <katul2469@gmail.com>
2026-08-26 16:31:14 +0530

    major update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -293,7 +293,7 @@ export default function Sidebar({
         </div>
 
         {/* LOGS SECTION ACCORDION */}
-        <div className="px-1 py-2 mt-2 space-y-2">
+        <div className="px-1 py-2 space-y-2">
           {/* Header Toggle */}
           <div
             onClick={() => setIsLogsOpen((prev) => !prev)}
