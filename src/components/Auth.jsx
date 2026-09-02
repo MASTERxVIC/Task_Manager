14b4e1593727fc0bb19eefc2f56503b10593f496
Atul Kumar <katul2469@gmail.com>
2026-09-02 21:01:48 +0530

    Final

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -58,9 +58,9 @@ export default function Auth() {
   };
 
   return (
-    <div className="min-h-screen w-full flex task-bg overflow-hidden fixed inset-0">
+    <div className="min-h-screen w-full flex  overflow-hidden fixed inset-0">
       {/* Left Side: Login / Signup Form */}
-      <div className="w-full lg:w-1/2 h-full flex flex-col justify-between p-8 sm:p-12 overflow-y-auto no-scrollbar bg-void z-10">
+      <div className="w-full lg:w-1/2 h-full flex flex-col justify-between p-8 sm:p-12 overflow-y-auto no-scrollbar task-bg z-10">
         <div className="max-w-md w-full mx-auto my-auto">
           <h2 className="text-3xl font-bold mb-2 text-surface">
             {isSignUp ? 'Create your free account' : 'Welcome Back'}
