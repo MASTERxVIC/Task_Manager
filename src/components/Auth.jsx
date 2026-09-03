224690be9f0402a03d12d0acd9fa0547e39ffa06
Atul Kumar <katul2469@gmail.com>
2026-09-03 11:50:26 +0530

    Final

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabaseClient';
 
   // Public folder wali images ka array yahan define karde bhai
