ce01c24c1252aeab353a7ef3678629d3e6f80cd3
Atul Kumar <katul2469@gmail.com>
2026-09-04 21:16:11 +0530

    Final

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -107,7 +107,7 @@ export default function Sidebar({
   };
 
   return (
-    <aside className="flex flex-col h-full w-full lg:w-[339px] shrink-0 bg-[#1E1E24] p-4 overflow-hidden select-none">
+    <aside className="flex flex-col h-full w-full lg:w-84.75 shrink-0 bg-[#1E1E24] p-4 overflow-hidden select-none">
       {/* Header - App Brand */}
       <div className="flex items-center justify-between mb-4 shrink-0 px-2">
         <div className="flex items-center gap-2">
